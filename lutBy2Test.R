5953e63d0f93e7fdb72abfffc2d84f2b2317d856
Sebastien Mella <sebmella@Sebastiens-MacBook-Pro.local>
2016-01-07 14:06:54 +0100

    modified lutBy2Test.R

--- lutBy2Test.R
+++ lutBy2Test.R
@@ -1,3 +1,3 @@
-load(https://github.com/sebmella/Repo-test1/commit/676508d2530804b5df08513b000a7e0b3d7c9d06)
+source(https://github.com/sebmella/Repo-test1/blob/master/multBy2.R)
 a <- 2
 b <- multipliePar2(a)
